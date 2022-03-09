7bfa0ae8177fc4455f4d767f28d52e2eef58b383
Nenad Savkic <savkicn@gmail.com>
2022-03-09 16:29:12 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.main')
+
+@section('title')
+    About Us
+@endsection
+
+@section('content')
+    
+   <div class="container">
+       <div class="row">
+           <div class="col-12">
+               <div class="section-heading">
+                   <div class="line-dec"></div>
+                   <h1>About Us</h1>
+                   <div class="row">
+                    <div class="col-md-6">
+                        <img src="{{ asset('/images/about-us.jpg') }}" class="img-fluid">
+                     </div>
+                     <div class="col-md-6">
+                         <h4>Something about Us</h4>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                         Vero ullam suscipit esse fugit repudiandae expedita dicta tempora,
+                         dolor soluta magnam facere nihil itaque doloribus quae vel. Aperiam alias illo reprehenderit!
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                         Vero ullam suscipit esse fugit repudiandae expedita dicta tempora,
+                         dolor soluta magnam facere nihil itaque doloribus quae vel. Aperiam alias illo reprehenderit!
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                         Vero ullam suscipit esse fugit repudiandae expedita dicta tempora,
+                         dolor soluta magnam facere nihil itaque doloribus quae vel. Aperiam alias illo reprehenderit!
+                        </p>
+                        <br>
+                        <p>Vero ullam suscipit esse fugit repudiandae expedita dicta tempora,
+                            dolor soluta magnam facere nihil itaque doloribus quae vel. Aperiam alias illo reprehenderit!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Vero ullam suscipit esse fugit repudiandae expedita dicta tempora,
+                            dolor soluta magnam facere nihil itaque doloribus quae vel. Aperiam alias illo reprehenderit!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Vero ullam suscipit esse fugit repudiandae expedita dicta tempora,
+                            dolor soluta magnam facere nihil itaque doloribus quae vel. Aperiam alias illo reprehenderit!
+                        </p>
+                        <br>
+                        <p>
+                            <a rel="nofolow" href="https://www.tooplate.com/view/2114-pixie">Pixie HTML template</a>
+                            can be converted into your desired CMS theme. You can use this Bootstrap v4.1.3 layout for 
+                            any online shop.Please tell your friends about <a rel="nofolow" href="https://facebook.com/tooplate">Tooplate</a>.
+                        Thank you.</p>
+
+                     </div>
+                   </div>
+                   
+
+               </div>
+           </div>
+       </div>
+   </div>
+
+@endsection
